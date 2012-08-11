a556878d014f2b766a5c5e675b93c35bb7030a8a
Grazielle Leotero <saziemucai@gmail.com>
2012-08-11 23:46:13 +0800

    first commit

---
+++ login-form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<link href="css/login.css" rel="stylesheet" type="text/css" media="screen" />
+<title>Login</title>
+<br><br><br><br><br><br><br><br>
+
+<body background="images/diagonal.jpg">
+<form>
+          <div class="box">
+            <h1>STUDENT'S LOGIN</h1>
+            <label>
+               <span>Username:</span>
+               <input type="text" class="input_text" name="username" id="username"/><br><br>
+            
+               <span>Password:</span>
+               <input type="text" class="input_text" name="password" id="password"/><br><br>
+			     <input type="button" class="button" value="LOGIN" style="font: bold 14px Arial"/>				 
+            </label>
+			
+             <label>
+			 <div class="left">
+               Need an account? <input type="button" class="submit" value="REGISTRATION" style="font: bold 14px Arial"/>
+			   </div>
+            </label>
+            
+             		         
+            
+         </div>
+    </form>
+
+</body>
+</html>
+
